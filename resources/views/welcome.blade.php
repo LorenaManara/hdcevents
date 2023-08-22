6d709738cc37360b9fd5b46dc5af77e1ad5d5830
LorenaManara <lomanara123@gmail.com>
2023-08-22 11:07:24 -0300

    Subindo update e delete que faltava do crud

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
                 <div class="card-body">
                     <p class="card-date">{{ date('d/m/y', strtotime($event->date)) }}</p>
                     <h5 class="card-title">{{ $event->title }}</h5>
-                    <p class="card-participantes">x participantes</p>
+                    <p class="card-participants">{{ count($event->users) }} {{ count($event->users) <= 1 ? 'Participante' : 'Participantes' }}</p>
                     <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,19 @@ Route::get('/',  [EventController::class, 'index']);
 Route::get('/events/create',  [EventController::class, 'create'])->middleware('auth'); //deixar so usuarios logados criar eventos
 Route::get('/events/{id}',  [EventController::class, 'show']);
 Route::post('/events',[EventController::class, 'store']);
-
-
-
+Route::delete('/events/{id}', [EventController::Class, 'destroy'])->middleware('auth');
+Route::get('/events/edit/{id}', [EventController::Class, 'edit'])->middleware('auth');
+Route::put('events/update/{id}', [EventController::Class, 'update'])->middleware('auth');
 
 Route::get('/contact', function () {
     return view('contact');
 });
 
 Route::get('/dashboard', [EventController::Class, 'dashboard'])->middleware('auth');
+
+Route::post('/events/join/{id}', [EventController::Class, 'joinEvent'])->middleware('auth');
+
+Route::delete('/events/leave/{id}', [EventController::Class, 'leaveEvent'])->middleware('auth');
 
 
 
